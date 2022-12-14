a287a9c477265ad396061b0164a43578291f94a1
rahulmaheshwari1111 <rahulmaheshwari240@gmail.com>
2022-12-14 14:16:37 +0530

    added chip story

---
+++ src/stories/Chip.stories.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Story } from "@storybook/react";
+import { Chip, IChipProps } from "../components/Chip";
+
+
+export default {
+  title: "Chip",
+  component: Chip,
+};
+
+const Template: Story<IChipProps> = args => <Chip {...args} />;
+
+export const basic = Template.bind({});
+basic.args = {
+    chipText: 'Your Text here..'
+};
+
